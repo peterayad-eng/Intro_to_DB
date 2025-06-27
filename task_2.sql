7d73104a8437844faca3070f520fd81540863658
Peter Ayad <eng.peter2013@gmail.com>
2025-06-27 13:46:20 -0700

    Create Your Magical Tables

---
+++ task_2.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Books (
+        book_id INT PRIMARY KEY,
+        title VARCHAR(130),
+        author_id INT FOREIGN KEY REFERENCES Authors(author_id),
+        price DOUBLE,
+        publication_date DATE
+);
+
+CREATE TABLE Authors (
+        author_id INT PRIMARY KEY,
+        author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+        customer_id INT PRIMARY KEY,
+        customer_name VARCHAR(215),
+        email VARCHAR(215),
+        address TEXT
+);
+
+CREATE TABLE Orders (
+        order_id INT PRIMARY KEY,
+        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+        order_date DATE
+);
+
+CREATE TABLE Order_Details (
+        orderdetail_id INT PRIMARY KEY,
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+        FOREIGN KEY (book_id) REFERENCES Books(book_id),
+        quantity DOUBLE
+);
+
