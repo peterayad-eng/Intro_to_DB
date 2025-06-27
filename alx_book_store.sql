77355713a9b411b29a9ccb6b25c6267572d165f7
Peter Ayad <eng.peter2013@gmail.com>
2025-06-27 02:14:28 -0700

    A Magical Database for Your Dream Online Bookstore Adventure

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -22,14 +22,14 @@ CREATE TABLE Customers (
 
 CREATE TABLE Orders (
 	order_id INT PRIMARY KEY,
-	customer_id INT FOREIGN KEY REFERENCES Customers(customer_id),
+	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 	order_date DATE
 );
 
 CREATE TABLE Order_Details (
 	orderdetail_id INT PRIMARY KEY,
-	order_id INT FOREIGN KEY REFERENCES Orders(order_id),
-	book_id INT FOREIGN KEY REFERENCES Books(book_id),
+	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+	FOREIGN KEY (book_id) REFERENCES Books(book_id),
 	quantity DOUBLE
 );
 
